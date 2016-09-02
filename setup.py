677a7130996a65cdef5fea39dcb92657758a16d4
Joshua Blake <joshua.blake@ocado.com>
2016-09-02 10:10:43 +0100

    Update requirements, tidy setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(name='bug_hunt',
-    install_requires = [
+setup(
+    name='bug_hunt',
+    install_requires=[
         'django>=1.8.0,<1.9',
-    ],
-    tests_require=[
-        'django-selenium-clean',
+        'selenium'
     ],
 )
